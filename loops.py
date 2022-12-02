8d406644a1f5df285f2e1e1c9f7c443155559944
isaackumi <isaac.kumi@ashesi.edu.gh>
2022-12-02 21:57:40 +0000

    feat: loops and string methods

---
+++ loops.py
@@ -0,0 +1,78 @@
+# Loops
+
+
+
+#Iterable
+# name_of_inst = input("Enter the name of your school: ")
+
+# # convert_to_upper = name_of_inst.lower()
+# # print(convert_to_upper)
+
+# if name_of_inst.upper().startswith("K"):
+#     print("Access Granted!")
+# else:
+#     print("Access Denied!")
+
+# password = input("Enter password: ")
+
+# if password.isalnum():
+#     print("Strong Password")
+# else:
+#     print("Weak password")
+
+# email  = "isaac.kumi@ashesi.edu.gh"
+#print(email.split("."))
+
+# stmt = "I like Python"
+
+# print(stmt.split())
+
+name = "J9u8s7t6i5c4e3A2p1p"
+#JusticeApp
+# a= name[0]
+# b = name[1]
+# c= name[2]
+# d = name[3]
+# e= name[4]
+# ........
+# z = name[18]
+
+result = ""
+for i in range(len(name)):
+    if i%2 ==0:
+        # print(name[i])
+        result+=name[i]
+print(result)
+    
+    
+
+
+
+
+
+
+for i in name:
+    pass
+
+    
+    
+# print(result)
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
